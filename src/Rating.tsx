620894ce74c74a93451faa5d961e20d1c50fe116
josh <postor@gmail.com>
2021-05-12 15:37:42 +0800

    basic

--- src/Rating.tsx
+++ src/Rating.tsx
@@ -1,13 +1,13 @@
 /*eslint-disable */
-import React, { FC } from "react";
-import { getBG, StarColors } from "./util";
+import React, { FC, useEffect, useRef } from "react";
+import { getBG, init, RSER_CLASSNAME, StarColors } from "./util";
 
 interface StyleProp {
   base?: {
     [key: string]: any;
   };
   baseBG?: string;
-  selectedBG?: string;
+  selectedBG?: (value: number) => string;
 }
 
 interface RatingProps {
@@ -20,53 +20,71 @@ interface RatingProps {
   step?: number;
 }
 
+interface StylingConfig {
+  max?: number,
+  height?: number,
+  thumbRadius?: number,
+  step?: number,
+  fontSize?: number
+}
+
 const Rating: FC<RatingProps> = ({
-  value = 1,
-  onChange = () => {},
-  max = 10,
-  step = 0.5,
+  value = 0,
+  onChange = () => { },
+  max = 5,
+  step = 1,
   height = 40,
   thumbRadius = 8,
   styling
 }) => {
-  const { base = {}, selectedBG = "red", baseBG = "blue" } =
-    styling || getStyling(max, height, thumbRadius, step);
+  useEffect(() => init(), [])
+  let ref = useRef(null)
+  const { base = {}, selectedBG = () => '', baseBG = "blue" } =
+    styling || getStyling({ max, height, thumbRadius, step });
   let style = {
     ...base,
-    backgroundImage: `${selectedBG},${baseBG}`,
-    backgroundPosition: `${height}px 0,${height * (1 + value)}px 0`
+    backgroundImage: `${selectedBG(value)},${baseBG}`,
+    backgroundPosition: `0 0,0 0`
   };
   return (
     <input
+      ref={ref}
+      className={RSER_CLASSNAME}
       style={style}
       type="range"
       max={max}
       min={1}
       value={value}
       onChange={(e) => onChange(parseFloat(e.target.value))}
+      // @ts-ignore
+      onClick={() => (!value) && onChange(parseFloat(ref.current.value))}
     />
   );
 };
 
 export default Rating;
 
-export function getStyling(
-  max: number,
-  height: number,
-  thumbRadius: number,
-  step: number
+export function getStyling(config: StylingConfig
 ): StyleProp {
-  let bgOpt = { width: height, fontSize: height * 0.8 };
+  const {
+    max = 5,
+    height = 40,
+    thumbRadius = 8,
+    step = 1,
+    fontSize = 40,
+  } = config
+  let bgOpt = { width: height, fontSize };
   return {
     base: {
       padding: `0 ${(height * step) / 2 - thumbRadius}px`,
       display: "inline-block",
       width: height * step * (max - 1) + thumbRadius * 2,
       height,
-      backgroundRepeat: "repeat-x",
-      appearance: "none"
+      backgroundRepeat: "no-repeat",
+      appearance: "none",
+      outline: "none",
     },
-    baseBG: getBG(bgOpt),
-    selectedBG: getBG({ ...bgOpt, fill: StarColors.selected })
+    baseBG: getBG({ ...bgOpt, totalWidth: height * max }),
+    selectedBG: value => getBG({ ...bgOpt, fill: StarColors.Selected, totalWidth: value * height * step })
   };
 }
--- src/util.ts
+++ src/util.ts
@@ -3,29 +3,53 @@ interface SvgConfig {
   fontSize?: number;
   fill?: string;
   text?: string;
+  totalWidth?: number
 }
 
 export enum StarColors {
-  hover = "#2196f3",
-  empty = "#555",
-  selected = "#ff912c"
+  Hover = "#2196f3",
+  Empty = "#555",
+  Selected = "#ff912c"
+}
+
+export const RSER_CLASSNAME = 'RSERating'
+let inited = false
+export function init() {
+  if (inited || typeof window == 'undefined') return
+  inited = true
+  let prefixes = [`-webkit-slider-`, `-moz-range-`]
+  addStyleToHead(`${prefixes.map(x =>
+    `.${RSER_CLASSNAME}::${x}thumb{opacity:0;}`)}`)
+}
+
+function addStyleToHead(css: string) {
+  var head = document.head || document.getElementsByTagName('head')[0],
+    style = document.createElement('style')
+  head.appendChild(style)
+  style.type = 'text/css';
+  style.appendChild(document.createTextNode(css))
 }
 
 function getSvg(config: SvgConfig = {}) {
-  let { width, fontSize, fill, text } = {
+  let { width, fontSize, fill, text, totalWidth } = {
     width: 20,
-    fontSize: 18,
-    fill: StarColors.empty,
+    fontSize: 20,
+    fill: StarColors.Empty,
     text: unescape(encodeURIComponent("★")),
+    totalWidth: 100,
     ...config
   };
-  return `<svg xmlns="http://www.w3.org/2000/svg" width="${width}px" height="${width}px"><text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle" fill="${fill}" style="font:${fontSize}px/${width}px serif">${text}</text></svg>`;
+  return `<svg xmlns="http://www.w3.org/2000/svg" width="${totalWidth}" height="${width
+    }"><symbol id="text" width="${width}" height="${width
+    }"><text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle" fill="${fill
+    }" style="font:${fontSize}px/${width}px serif">${text
+    }</text></symbol>${new Array(Math.ceil(totalWidth / width)).fill(0).map((x, i) =>
+      `<use href="#text" x="${i * width}"/>`).join('')}</svg>`
 }
 
 function ismoficBtoa(str: string) {
-  if (typeof window !== "undefined") return window.btoa(str);
-  // @ts-ignore
-  return Buffer.from(str).toString("base64");
+  if (typeof window !== "undefined") return window.btoa(str)
+  return Buffer.from(str).toString("base64")
 }
 export function getBG(config: SvgConfig) {
   return `url("data:image/svg+xml;base64,${ismoficBtoa(getSvg(config))}")`;
